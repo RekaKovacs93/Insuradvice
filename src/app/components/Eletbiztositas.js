dcc7e3984a5a751ffcde845261539729b91a8084
Reka <reka.kovacs1993@gmail.com>
2024-01-23 18:26:28 +0000

    hexagons

--- src/app/components/Eletbiztositas.js
+++ src/app/components/Eletbiztositas.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-const servicesData = [
+const row1 = [
+
     {
         title: "Kockázati életbiztosítás",
         description: "Más néven haláleseti életbiztosítás: a biztosítótársaság meghatározott díj ellenében kötelezettséget vállal arra a szerződőnek, hogy a biztosított halála esetén a kedvezményezettnek kifizeti a haláleseti/biztosítási összeget.",
@@ -10,17 +11,27 @@ const servicesData = [
         title: "Megtakarítással összekötött életbiztosítás",
         description: "Abban különbözik a kockázati életbiztosítástól, hogy nem csak halálesetre fizet, hanem rokkantsági térítést is, illetve ellátnak befektetés általi tőkegyarapítást is.",
 
-    },
+    }
+  ]
+const row2 = [
     {
-        title: "Befektetési egységekhez kötött életbiztosítás",
+        title: "Befektetési életbiztosítás",
         description: "A megtakarításos életbiztosítás egyik fajtája, nem garantált a hozam, ennek ellenére a hozampotenciál nagyobb ennél a fajta biztosításnál, mint a vegyes életbiztosításnál, ez a fajta életbiztosítás CSAK tanácsadással történhet",
 
+    },
+    {
+      title: "Nyugdíj biztosítás",
+      description: "Olyan életbiztosítás, amikor a szerződő egy vállalat, társaság, cég stb. lehet, aki becsatlakozik az válhat biztosítottá",
+      
     },
     {
         title: "Vegyes életbiztosítás",
         description: "Megtakarításos életbiztosítás másik fajtája, garantált kamat, ami egy megadott értéknél lehet több, de kevesebb sosem. 2021-ben 424.528 ilyen szerződés talált gazdára.",
 
     },
+
+  ]
+const row3 = [
     {
       title: "Euróalapú megtakarításos életbiztosítás",
       description: "Annak ajánlom, aki szeretne értékvédelmet a megtakarításának és a vásárlóértékének növelése a megtakarítási idő alatt",
@@ -36,38 +47,90 @@ const servicesData = [
 export default function Eletbiztositas() {
   return (
     <div className="flex flex-col items-center w-full justify-center min-h-screen">
-      <h1 className="text-center  text-5xl px-10 md:py-5 py-3 my-20">Életbiztosítás és azok fajtái:</h1>
+      <h1 className="text-center  text-5xl px-10 md:py-5 py-3 my-20">Biztosítások fajtái</h1>
       {/* <h1 className="text-center md:text-xl md:mb-14 md:px-10">Költségei a mi pénztárcánkat terhelik!</h1> */}
 
       {/* Desktop layout */}
-      <div className="hidden sm:flex flex-col sm:flex-row flex-wrap justify-center gap-x-10 gap-y-5 items-center mx-40">
-        {servicesData.map((service, index) => (
+      <div className="hidden sm:flex flex-col sm:flex-row flex-wrap justify-center gap-x-8 items-center ">
+        {row1.map((service, index) => (
           <div key={index} className=" items-center ">
-            <div className="h-48 w-48 md:h-64 md:w-64 relative ">
-            {/* <div className="hexagon "> */}
-              <Image
-                src="https://cdn.pixabay.com/photo/2020/04/12/18/43/abstract-5035370_1280.jpg"
+            <div className="relative ">
+            <div className="hexagon">
+              {/* <Image
+                src="/hexagon.svg"
                 alt={service.title}
-                layout="fill"
-                className="rounded-lg object-cover opacity-20 hover:border-4 dark:border-accent border-accentLight bg-light dark:bg-dark"
-              />
-              <div id="card" className="absolute hover:border-2 dark:border-accent border-accentLight  inset-0 rounded-lg flex flex-col items-center justify-center">
-                <h1 id="title" className="absolute text-xl opacity-100 text-center px-5">{service.title}</h1>
+                width={300}
+                height={250}
+                className="rounded-lg object-cover opacity-20 rotate-90"
+              /> */}
+              <div id="card" className="absolute inset-0 rounded-lg flex flex-col items-center justify-center">
+                <h1 id="title" className="absolute text-2xl opacity-100 text-center px-5">{service.title}</h1>
                 <div id="desc" className="opacity-0">
-                  <h1 id="hovertitle" className="text-center text-xl  p-5">{service.title}</h1>
-                  <p className=" text-xs text-center px-5">{service.description}</p>
+                  <h1 id="hovertitle" className="text-center text-lg"><b>{service.title}</b></h1>
+                  <p className=" text-xs text-center ">{service.description}</p>
                 </div>
               </div>
 
-            {/* </div> */}
+            </div>
             </div>
           </div>
         ))}
+        
+      </div>
+      <div className="hidden sm:flex flex-col sm:flex-row flex-wrap justify-center gap-x-8 items-center mx-40 relative top-[-50px]">
+        {row2.map((service, index) => (
+          <div key={index} className=" items-center ">
+            <div className="relative ">
+            <div className="hexagon ">
+              {/* <Image
+                src="https://cdn.pixabay.com/photo/2020/04/12/18/43/abstract-5035370_1280.jpg"
+                alt={service.title}
+                layout="fill"
+                className="rounded-lg object-cover opacity-20"
+              /> */}
+              <div id="card" className="absolute   inset-0 rounded-lg flex flex-col items-center justify-center">
+                <h1 id="title" className="absolute text-2xl opacity-100 text-center px-5">{service.title}</h1>
+                <div id="desc" className="opacity-0">
+                  <h1 id="hovertitle" className="text-center text-lg"><b>{service.title}</b></h1>
+                  <p className=" text-xs text-center">{service.description}</p>
+                </div>
+              </div>
+
+            </div>
+            </div>
+          </div>
+        ))}
+        
+      </div>
+      <div className="hidden sm:flex flex-col sm:flex-row flex-wrap justify-center gap-x-8 items-center mx-40 relative top-[-100px]">
+        {row3.map((service, index) => (
+          <div key={index} className=" items-center ">
+            <div className="relative ">
+            <div className="hexagon">
+              {/* <Image
+                src="https://cdn.pixabay.com/photo/2020/04/12/18/43/abstract-5035370_1280.jpg"
+                alt={service.title}
+                layout="fill"
+                className="rounded-lg object-cover opacity-20 Light"
+              /> */}
+              <div id="card" className="absolute   inset-0 rounded-lg flex flex-col items-center justify-center">
+                <h1 id="title" className="absolute text-2xl opacity-100 text-center px-5">{service.title}</h1>
+                <div id="desc" className="opacity-0">
+                  <h1 id="hovertitle" className="text-center text-lg"><b>{service.title}</b></h1>
+                  <p className=" text-xs text-center ">{service.description}</p>
+                </div>
+              </div>
+
+            </div>
+            </div>
+          </div>
+        ))}
+        
       </div>
 
       {/* Mobile layout */}
       <div className="flex flex-col sm:hidden gap-10 px-10 py-5 ">
-        {servicesData.map((service, index) => (
+        {row1.map((service, index) => (
           <div key={index} className="border rounded-lg">
             <div className="h-48 relative rounded-lg  dark:border-accent border-accentLight bg-light dark:bg-dark">
               {/* <Image
